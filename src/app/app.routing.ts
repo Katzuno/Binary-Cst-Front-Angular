cc8f4f1c420c830ad7b6861ccfb7a01317a377df
Adrian <adriandinca2008@yahoo.com>
2019-11-16 17:34:06 +0200

    chore: Remove stot from routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,38 +1,37 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 
-import {AdminLayoutComponent} from './layouts/admin/admin-layout.component';
-import {AuthLayoutComponent} from './layouts/auth/auth-layout.component';
-import {CameraComponent} from 'src/app/pages-blank/camera/camera.component';
-import {StotComponent} from 'src/app/pages-blank/stot/stot.component';
+import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
+import { CameraComponent } from 'src/app/pages-blank/camera/camera.component';
 
 export const AppRoutes: Routes = [
-    {
-        path: '',
-        redirectTo: 'camera',
-        pathMatch: 'full',
-    }, {
-        path: 'dashboard',
-        component: AdminLayoutComponent,
-        children: [
-            {
-                path: '',
-                loadChildren: './dashboard/dashboard.module#DashboardModule'
-            }
-        ]
-    }, {
-        path: 'pages',
-        component: AuthLayoutComponent,
-        children: [{
-            path: '',
-            loadChildren: './pages/pages.module#PagesModule'
-        }]
-    },
-    {
-        path: 'camera',
-        component: CameraComponent
-    },
-    {
-        path: 'stot',
-        component: StotComponent
-    }
+	{
+		path: '',
+		redirectTo: 'camera',
+		pathMatch: 'full',
+	},
+	{
+		path: 'dashboard',
+		component: AdminLayoutComponent,
+		children: [
+			{
+				path: '',
+				loadChildren: './dashboard/dashboard.module#DashboardModule',
+			},
+		],
+	},
+	{
+		path: 'pages',
+		component: AuthLayoutComponent,
+		children: [
+			{
+				path: '',
+				loadChildren: './pages/pages.module#PagesModule',
+			},
+		],
+	},
+	{
+		path: 'camera',
+		component: CameraComponent,
+	},
 ];
