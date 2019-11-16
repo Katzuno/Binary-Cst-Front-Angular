f44aa2197796d7f8da4ec604e51d36ed4eb05935
Adrian <adriandinca2008@yahoo.com>
2019-11-16 13:03:11 +0200

    feat: Add websocket

---
+++ src/app/config/api.ts
@@ -0,0 +1,3 @@
+export const API = {
+	webSocketUrl: 'ws://localhost:3030',
+};
--- src/app/pages-blank/camera/camera.component.ts
+++ src/app/pages-blank/camera/camera.component.ts
@@ -1,16 +1,58 @@
-import {Component, OnInit} from '@angular/core';
+import { API } from './../../config/api';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-    selector: 'app-camera',
-    templateUrl: './camera.component.html',
-    styleUrls: ['./camera.component.css']
+	selector: 'app-camera',
+	templateUrl: './camera.component.html',
+	styleUrls: ['./camera.component.css'],
 })
 export class CameraComponent implements OnInit {
+	@ViewChild('videoRoot', { static: true })
+	videoElement: ElementRef;
 
-    constructor() {
-    }
+	canvas: HTMLCanvasElement;
 
-    ngOnInit() {
-    }
+	canvasContext: CanvasRenderingContext2D;
 
+	webSocket: WebSocket;
+
+	constructor() {}
+
+	async ngOnInit() {
+		this.initCanvas();
+
+		this.initWebSocket();
+
+		this.initStream();
+	}
+
+	initCanvas() {
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = window.innerWidth;
+		this.canvas.height = window.innerHeight;
+		this.canvasContext = this.canvas.getContext('2d');
+	}
+
+	initWebSocket() {
+		this.webSocket = new WebSocket(API.webSocketUrl);
+	}
+
+	async initStream() {
+		const stream = await navigator.mediaDevices.getUserMedia({
+			video: { width: window.innerWidth, height: window.innerHeight },
+		});
+
+		this.webSocket.addEventListener('open', () => {
+			requestAnimationFrame(() => {
+				this.webSocket.send(this.getFrame());
+			});
+		});
+	}
+
+	getFrame() {
+		this.canvasContext.drawImage(this.videoElement.nativeElement, 0, 0);
+		const image = this.canvas.toDataURL('image/png');
+		console.log(image);
+		return image;
+	}
 }
